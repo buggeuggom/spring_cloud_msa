deb559210a90a5019f03e76e66e8a88704306227
InKyuHwang001 <hwanginkyu42@naver.com>
2025-02-22 20:09:24 +0900

    다지우고 다시

--- user-service/src/main/java/com/ecomerce/userservice/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecomerce.userservice.repository;
-
-import com.ecomerce.userservice.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- user-service/src/main/java/com/ecomerce/userservice/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.ecomerce.userservice.service;
-
-import com.ecomerce.userservice.controller.request.RequestUser;
-import com.ecomerce.userservice.dto.UserDto;
-
-public interface UserService {
-
-    UserDto createUser(RequestUser request);
-}
